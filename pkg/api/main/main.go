1fae02ebf80a9bf3ecfe2eda2ea2e5b8ec944da4
js103 <643929860@qq.com>
2019-01-20 22:04:37 +0700

    整体架构过程处理

--- pkg/api/main/main.go
+++ pkg/api/main/main.go
@@ -24,3 +24,8 @@ func main() {
 	r:= RegisterHandlers()
 	http.ListenAndServe(":8000", r)
 }
+
+//过程
+
+// 从 handler 进来，验证用户名及密码，返回response
+// 从 main -> handler -> dbops调用，拿到信息，结合defs再做进一步处理（消息定义），组装成response后返回
