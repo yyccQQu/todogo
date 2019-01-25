dc8dd21965af7741ba08805f082394ea12fdaf3a
js103 <643929860@qq.com>
2019-01-25 22:22:11 +0700

    sheduler init

---
+++ pkg/scheduler/main.go
@@ -0,0 +1,10 @@
+package scheduler
+
+// 什么是sheduler
+// 叫做 任务，通过普通resetapi方法 不会马上给他结果的任务，
+// 这些任务会分发到 scheduler 里面 ，然后通过一些方法定时或者延时触发，settimeout ，interva 《异步任务》
+
+// 延迟操作，需要scheduler，周期，顺时
+
+//scheduler 包含RESTful 的 http server, Timer, task Runer
+
